b7b7381833348d6035667ead986bc4b709d43c1b
lucascorreagoldani <lucascorreagoldani04@gmail.com>
2025-08-17 00:09:44 -0300

    Error when a player opened the /toggle menu, the menu was opened to all online players

--- src/main/java/br/com/realmmc/core/listeners/OpenGuiListener.java
+++ src/main/java/br/com/realmmc/core/listeners/OpenGuiListener.java
@@ -5,9 +5,12 @@ import br.com.realmmc.core.api.CoreAPI;
 import br.com.realmmc.core.gui.profile.PreferencesGUI;
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteStreams;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
 
 public class OpenGuiListener implements PluginMessageListener {
 
@@ -18,17 +21,22 @@ public class OpenGuiListener implements PluginMessageListener {
     }
 
     @Override
-    public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, @NotNull byte[] message) {
+    public void onPluginMessageReceived(@NotNull String channel, @NotNull Player anyPlayer, @NotNull byte[] message) {
         if (!channel.equals("proxy:opengui")) return;
 
         ByteArrayDataInput in = ByteStreams.newDataInput(message);
         String subChannel = in.readUTF();
 
         if (subChannel.equals("OpenPreferences")) {
-            plugin.getServer().getScheduler().runTask(plugin, () -> {
-                CoreAPI.getInstance().getSoundManager().playClick(player);
-                new PreferencesGUI(player).open();
-            });
+            UUID targetUuid = UUID.fromString(in.readUTF());
+            Player targetPlayer = Bukkit.getPlayer(targetUuid);
+
+            if (targetPlayer != null && targetPlayer.isOnline()) {
+                plugin.getServer().getScheduler().runTask(plugin, () -> {
+                    CoreAPI.getInstance().getSoundManager().playClick(targetPlayer);
+                    new PreferencesGUI(targetPlayer).open();
+                });
+            }
         }
     }
 }
